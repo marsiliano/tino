f140d0bb0035cd99f74c804f3b3f397375600e6c
Federico Guerinoni <guerinoni@micro-systems.it>
2019-07-02 16:11:35 +0200

    menu: introduced quit action

---
+++ MainWindow.cpp
@@ -0,0 +1,21 @@
+#include "MainWindow.hpp"
+
+#include "ui_MainWindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent), ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    auto file = new QMenu("File", ui->menuBar);
+    auto quit = new QAction("Quit", file);
+    connect(quit, &QAction::triggered, this, []() { qApp->quit(); });
+    file->addAction(quit);
+
+    ui->menuBar->addMenu(file);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
