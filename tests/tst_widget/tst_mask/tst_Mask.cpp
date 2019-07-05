bc9805fc8462b1d179ecf5a6528d9b0055850a0e
Stivvo <entattis15@itisvinci.com>
2019-07-05 11:21:56 +0200

    test extra attributes, methods and initTestCase

--- tests/tst_widget/tst_mask/tst_Mask.cpp
+++ tests/tst_widget/tst_mask/tst_Mask.cpp
@@ -1,16 +1,56 @@
 #include "../../../src/widget/Mask.hpp"
 
+#include <QDebug>
 #include <QtTest>
 
 class tst_Mask : public QObject
 {
     Q_OBJECT
 
+  private:
+    QString expr;
+    QString expb;
+    Mask *m;
+
+    QString getClr(const int i);
+
   private slots:
+    void initTestCase();
     void tst_color();
+    void tst_click();
 };
 
+void tst_Mask::initTestCase()
+{
+    expr = "background-color:#ff0000";
+    expb = "background-color:#0000ff";
+
+    std::vector<bool> v = {
+        true, false, true, false, true, false, true, false
+    };
+    std::vector<std::string> s = { "prova" };
+    core::Byte b(v, s, false);
+    m = new Mask(b, nullptr);
+}
+
+QString tst_Mask::getClr(const int i)
+{
+    QString cmp = m->b[i]->styleSheet();
+    cmp         = cmp.mid(cmp.indexOf("background-color:"), expr.length());
+    return cmp;
+}
+
 void tst_Mask::tst_color()
+{
+    for (int i = 0; i < 8; ++i) {
+        if (i % 2 == 0)
+            QCOMPARE(getClr(i), expr);
+        else
+            QCOMPARE(getClr(i), expb);
+    }
+}
+
+void tst_Mask::tst_click()
 {
     std::vector<bool> v = {
         true, false, true, false, true, false, true, false
@@ -19,19 +59,9 @@ void tst_Mask::tst_color()
     core::Byte b(v, s, false);
     Mask m(b, nullptr);
 
-    QColor clrr(255, 0, 0);
-    QColor clrb(0, 0, 255);
-
-    for (int i = 0; i < 8; i++) {
-        QColor clr = m.b[i]->palette().button().color();
-
-        if (i % 2 == 0)
-            QCOMPARE(clr, clrr);
-        else
-            QCOMPARE(clr, clrb);
+    for (int i = 0; i < 8; ++i) {
     }
 }
-
 QTEST_MAIN(tst_Mask)
 
 #include "tst_Mask.moc"
