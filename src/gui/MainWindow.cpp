44cb8eef306c7db878c91fbd09f7dcea66210b61
Stivvo <entattis15@itisvinci.com>
2019-07-10 12:00:33 +0200

    yaml parsing doesn't work

--- src/gui/MainWindow.cpp
+++ src/gui/MainWindow.cpp
@@ -51,55 +51,5 @@ std::vector<core::Block> MainWindow::parse(std::string flname)
 {
     std ::vector<core::Block> all;
 
-    YAML::Node base      = YAML::LoadFile(flname);
-    YAML::Node blocklist = base["blocks"]; // get block names
-
-    for (int i = 0; i < blocklist.size(); ++i) {
-        YAML::Node blockNode =
-            base[blocklist[i].as<std::string>()]; // get the block ==
-        // names
-
-        std::vector<core::Group> g;
-
-        for (int j = 0; j < blockNode.size(); ++j) {
-            YAML::Node groupNode =
-                blockNode[blockNode[i].as<std::string>()]; // get the
-                                                           // group
-            YAML::Node bytesNode = groupNode["bytes"];
-
-            std::vector<core::Byte> b;
-
-            bool rw;
-            std::vector<bool> v;
-            std::vector<std::string> s;
-
-            for (int k = 0; k < bytesNode.size(); ++k) {
-                switch (k % 3) {
-                    case 0:
-                        rw = bytesNode[j];
-                        break;
-                    case 1:
-                        v.clear();
-                        for (int l = 0; l < bytesNode[j].size(); ++l) {
-                            v.push_back(bytesNode[j][k].as<bool>());
-                        }
-                        break;
-                    case 2:
-                        s.clear();
-                        for (int l = 0; l < bytesNode[j].size(); ++l) {
-                            s.push_back(bytesNode[j][k].as<std::string>());
-                        }
-                        core::Byte tb(v, s, rw);
-                        b.push_back(tb);
-                        break;
-                } // end switch
-            }
-            core::Group tg(b, groupNode["type"].as<char>());
-            g.push_back(tg);
-        } // end (int j = 0; j < blockNode.size(); ++j)
-        core::Block tblock(g, blocklist[i].as<std::string>());
-        all.push_back(tblock);
-    }
-
     return all;
 }
