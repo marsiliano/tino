168fb2f5f7579fef4d8d0891e92033c7ad8a85be
Federico Guerinoni <guerinoni@micro-systems.it>
2019-11-20 09:57:42 +0100

    Add test with simple protocol

--- tests/tst_gui/tst_configView/tst_ConfigView.cpp
+++ tests/tst_gui/tst_configView/tst_ConfigView.cpp
@@ -12,6 +12,7 @@ class tst_ConfigView : public QObject
   private slots:
     void initTestCase();
     void checkEmptyProtocol();
+    void checkSimpleProtocol();
 
   private:
     Protocol m_protocol;
@@ -19,18 +20,49 @@ class tst_ConfigView : public QObject
 
 void tst_ConfigView::initTestCase()
 {
-    //    m_protocol.blocks.push_back();
+    Byte b;
+    b.description = "byte";
+    for (auto i = 0; i < 8; ++i) {
+        Flag f;
+        f.description = "flag " + QString::number(i);
+        b.flags.push_back(f);
+    }
+
+    Group g;
+    g.description = "group";
+    g.bytes.push_back(b);
+
+    Block bl;
+    bl.description = "block";
+    bl.groups.push_back(g);
+
+    m_protocol.blocks.push_back(bl);
 }
 
 void tst_ConfigView::checkEmptyProtocol()
 {
-    auto dock = ConfigViewFactory().makeConfigView({});
-    QVERIFY(dock != Q_NULLPTR);
-    const auto view = qobject_cast<QTreeView *>(dock->widget());
-    QVERIFY(view != Q_NULLPTR);
+    auto dock        = ConfigViewFactory().makeConfigView({});
+    const auto view  = qobject_cast<QTreeView *>(dock->widget());
     const auto model = qobject_cast<QStandardItemModel *>(view->model());
+    QVERIFY(dock != Q_NULLPTR);
+    QVERIFY(view != Q_NULLPTR);
     QVERIFY(model != Q_NULLPTR);
     QVERIFY(model->rowCount() == 0);
+}
+
+void tst_ConfigView::checkSimpleProtocol()
+{
+    auto dock        = ConfigViewFactory().makeConfigView(m_protocol);
+    const auto view  = qobject_cast<QTreeView *>(dock->widget());
+    const auto model = qobject_cast<QStandardItemModel *>(view->model());
+    QVERIFY(dock != Q_NULLPTR);
+    QVERIFY(view != Q_NULLPTR);
+    QVERIFY(model != Q_NULLPTR);
+    QVERIFY(model->rowCount() == 1);
+
+    auto root = model->data(model->index(0, 0));
+    QVERIFY(root.isValid());
+    QCOMPARE(root.toString(), "block");
 }
 
 QTEST_MAIN(tst_ConfigView)
