28b6dd088e01e89e9813be8e400ed11c95b493aa
Federico Guerinoni <guerinoni@micro-systems.it>
2019-11-20 08:28:54 +0100

    Complete generate ConfigView

--- src/gui/ConfigViewFactory.cpp
+++ src/gui/ConfigViewFactory.cpp
@@ -8,15 +8,31 @@ QDockWidget *ConfigViewFactory::makeConfigView(const Protocol &prot) const
 {
     auto dock = new QDockWidget("Protocol");
     dock->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
-    auto model                = new QStandardItemModel(dock);
-    QStandardItem *parentItem = model->invisibleRootItem();
-    for (int i = 0; i < 4; ++i) {
-        QStandardItem *item = new QStandardItem(QString("item %0").arg(i));
-        parentItem->appendRow(item);
-        parentItem = item;
+    auto view  = new QTreeView(dock);
+    auto model = new QStandardItemModel(view);
+
+    const auto root = model->invisibleRootItem();
+    foreach (auto b, prot.blocks) {
+        auto blockItem = new QStandardItem(b.description);
+        blockItem->setFlags(blockItem->flags() & ~Qt::ItemIsEditable);
+        foreach (auto g, b.groups) {
+            auto groupItem = new QStandardItem(g.description);
+            groupItem->setFlags(groupItem->flags() & ~Qt::ItemIsEditable);
+            foreach (auto b, g.bytes) {
+                auto byteItem = new QStandardItem(b.description);
+                byteItem->setFlags(byteItem->flags() & ~Qt::ItemIsEditable);
+                foreach (auto f, b.flags) {
+                    auto flagItem = new QStandardItem(f.description);
+                    flagItem->setFlags(flagItem->flags() & ~Qt::ItemIsEditable);
+                    byteItem->appendRow(flagItem);
+                }
+                groupItem->appendRow(byteItem);
+            }
+            blockItem->appendRow(groupItem);
+        }
+        root->appendRow(blockItem);
     }
 
-    auto view = new QTreeView(dock);
     view->setModel(model);
     dock->setWidget(view);
     return dock;
