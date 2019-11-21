5ffaa9e343562397095240ddeb2701afac639fc8
Federico Guerinoni <guerinoni@micro-systems.it>
2019-11-21 11:51:00 +0100

    Add test when import go well

--- tests/tst_gui/tst_mainWindow/tst_MainWindow.cpp
+++ tests/tst_gui/tst_mainWindow/tst_MainWindow.cpp
@@ -6,17 +6,36 @@ class tst_MainWindow : public QObject
     Q_OBJECT
 
   private slots:
+    void initTestCase();
     void doNothingWhenImportFilenameEmpty();
+    void emitWhenImportLooksFine();
 
   private:
     MainWindow m_mainWindow;
+    QString filesPath;
 };
+
+void tst_MainWindow::initTestCase()
+{
+    auto path = QDir::currentPath();
+    path      = path.remove(path.lastIndexOf("build"), path.size());
+    path += "tests/files/";
+    filesPath = path;
+}
 
 void tst_MainWindow::doNothingWhenImportFilenameEmpty()
 {
     QSignalSpy spyImportFinished(&m_mainWindow, &MainWindow::importFinished);
     m_mainWindow.importConfig({});
     QCOMPARE(spyImportFinished.count(), 0);
+}
+
+void tst_MainWindow::emitWhenImportLooksFine()
+{
+    QSignalSpy spyImportFinished(&m_mainWindow, &MainWindow::importFinished);
+    m_mainWindow.importConfig(filesPath + "1block-1group-flag.json");
+    QCOMPARE(spyImportFinished.count(), 1);
+    QVERIFY(!m_mainWindow.m_config.isNull());
 }
 
 QTEST_MAIN(tst_MainWindow)
